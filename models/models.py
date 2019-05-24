ba6b45fb2fa3b415ac85852ad5f1a4d941278b3b
Milos <milos.vujasinovic.98@gmail.com>
2019-05-24 21:22:03 +0200

    All model constraints moved to __table_args__

--- models/models.py
+++ models/models.py
@@ -1,16 +1,23 @@
 from database import Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.schema import PrimaryKeyConstraint, ForeignKeyConstraint, UniqueConstraint
 
 import datetime
 
 class Korisnik(Base):
 	__tablename__ = 'korisnik'
 
-	id = Column(Integer, primary_key=True)
-	username = Column(String(30), nullable=False, unique=True, index=True)
+	id = Column(Integer)
+	username = Column(String(30), nullable=False, index=True)
 	password = Column(String(200), nullable=False)
 	uloga = Column(String(20), nullable=False)
+
+	__table_args__ = (
+		PrimaryKeyConstraint(id),
+		UniqueConstraint(username),
+		{}
+	)
 
 	__mapper_args__ = {
 		'polymorphic_identity':'korisnik',
@@ -28,7 +35,13 @@ class Korisnik(Base):
 class Admin(Korisnik):
 	__tablename__ = 'admin'
 
-	id = Column(Integer, ForeignKey('korisnik.id'), primary_key=True)
+	id = Column(Integer)
+
+	__table_args__ = (
+		ForeignKeyConstraint([id], [Korisnik.id]),
+		PrimaryKeyConstraint(id),
+		{}
+	)
 
 	__mapper_args__ = {
 		'polymorphic_identity':'admin'
@@ -41,9 +54,15 @@ class Admin(Korisnik):
 class Ucenik(Korisnik):
 	__tablename__ = 'ucenik'
 
-	id = Column(Integer, ForeignKey('korisnik.id'), primary_key=True)
+	id = Column(Integer)
 	ime = Column(String(30), nullable=False)
 	prezime = Column(String(30), nullable=False)
+
+	__table_args__ = (
+		ForeignKeyConstraint([id], [Korisnik.id]),
+		PrimaryKeyConstraint(id),
+		{}
+	)
 
 	__mapper_args__ = {
 		'polymorphic_identity':'ucenik'
@@ -61,9 +80,16 @@ class Ucenik(Korisnik):
 class Profesor(Korisnik):
 	__tablename__ = 'profesor'
 
-	id = Column(Integer, ForeignKey('korisnik.id'), primary_key=True)
+	id = Column(Integer)
 	ime = Column(String(30), nullable=False)
 	prezime = Column(String(30), nullable=False)
+	predmeti = relationship('Predmet', secondary='predaje')
+
+	__table_args__ = (
+		ForeignKeyConstraint([id], [Korisnik.id]),
+		PrimaryKeyConstraint(id),
+		{}
+	)
 
 	__mapper_args__ = {
 		'polymorphic_identity':'profesor'
@@ -81,8 +107,15 @@ class Profesor(Korisnik):
 class Predmet(Base):
 	__tablename__ = 'predmet'
 
-	id = Column(Integer, primary_key=True)
-	naziv = Column(String(50), unique=True, nullable=False)
+	id = Column(Integer)
+	naziv = Column(String(50), nullable=False)
+	profesori = relationship('Profesor', secondary='predaje')
+
+	__table_args__ = (
+		PrimaryKeyConstraint(id),
+		UniqueConstraint(naziv),
+		{}
+	)
 
 	def __init__(self, naziv):
 		self.naziv = naziv
@@ -94,8 +127,15 @@ class Predmet(Base):
 class Predaje(Base):
 	__tablename__ = 'predaje'
 
-	profesor_id = Column(Integer, ForeignKey('profesor.id', ondelete='CASCADE'), primary_key=True)
-	predmet_id = Column(Integer, ForeignKey('predmet.id', ondelete='CASCADE'), primary_key=True)
+	profesor_id = Column(Integer)
+	predmet_id = Column(Integer)
+
+	__table_args__ = (
+		ForeignKeyConstraint([profesor_id], [Profesor.id]), # add on delete cascade
+		ForeignKeyConstraint([predmet_id], [Predmet.id]), # add on delete cascade
+		PrimaryKeyConstraint(profesor_id, predmet_id),
+		{}
+	)
 
 	def __repr__(self):
 		return f'<Predaje()>'
@@ -104,22 +144,37 @@ class Predaje(Base):
 class Slusa(Base):
 	__tablename__ = 'slusa'
 
-	ucenik_id = Column(Integer, ForeignKey('ucenik.id', ondelete='CASCADE'), primary_key=True)
-	predmet_id = Column(Integer, ForeignKey('predmet.id', ondelete='CASCADE'), primary_key=True)
-	predaje_profesor_id = Column(Integer, ForeignKey('predaje.profesor_id', ondelete='SET NULL'))
-	predaje_predmet_id = Column(Integer, ForeignKey('predaje.predmet_id', ondelete='SET NULL'))
+	ucenik_id = Column(Integer)
+	predmet_id = Column(Integer)
+	predaje_profesor_id = Column(Integer)
+	predaje_predmet_id = Column(Integer)
+
+	__table_args__ = (
+		ForeignKeyConstraint([ucenik_id], [Ucenik.id]), # add on delete cascade
+		ForeignKeyConstraint([predmet_id], [Predmet.id]), # add on delete cascade
+		ForeignKeyConstraint([predaje_profesor_id, predaje_predmet_id], [Predaje.profesor_id, Predaje.predmet_id]), # add on delete set null
+		PrimaryKeyConstraint(ucenik_id, predmet_id),
+		{}
+	)
 
 	def __repr__(self):
 		return f'<Slusa()>'
 
+
 class Ocena(Base):
 	__tablename__ = 'ocena'
 
-	slusa_ucenik_id = Column(Integer, ForeignKey('slusa.ucenik_id', ondelete='CASCADE'), primary_key=True)
-	slusa_predmet_id = Column(Integer, ForeignKey('slusa.predmet_id', ondelete='CASCADE'), primary_key=True)
-	ocena_id = Column(Integer, primary_key=True)
+	slusa_ucenik_id = Column(Integer)
+	slusa_predmet_id = Column(Integer)
+	ocena_id = Column(Integer)
 	datum = Column(Date, default=datetime.datetime.now)
 	vrednost = Column(Integer, nullable=False)
+
+	__table_args__ = (
+		ForeignKeyConstraint([slusa_ucenik_id, slusa_predmet_id], [Slusa.ucenik_id, Slusa.predmet_id]), # add on delete cascade
+		PrimaryKeyConstraint(slusa_ucenik_id, slusa_predmet_id, ocena_id),
+		{}
+	)
 
 	def __repr__(self):
 		return f'<Ocena()>'

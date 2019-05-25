6add9276f5675293cf79300f3531a2d64e305958
mvujas <milos.vujasinovic.98@gmail.com>
2019-05-26 00:00:33 +0200

    init script added and shared functionality to views

---
+++ init.py
@@ -0,0 +1,61 @@
+import argparse
+from database import init_db
+from businesslayer import KorisnikLogic, InvalidKorisnikInfoError
+import getpass
+from config import LOCAL_DATABASE_NAME
+import os
+
+def main():
+	parser = argparse.ArgumentParser(description='Initialize database')
+	parser.add_argument('--auto', dest='auto_init', action='store_true',
+						help='Automatically initialize the system')
+	parser.set_defaults(auto_init=False)
+	args = parser.parse_args()
+
+	try:
+		if os.path.exists(LOCAL_DATABASE_NAME):
+			if not args.auto_init:
+				hard_reset = input('Baza vec postoji, zelite li da je obrisete i krenete iz pocetka [D/n] ')\
+					.strip().upper() not in ['N', 'NE']
+				if not hard_reset:
+					raise Exception()
+			os.remove(LOCAL_DATABASE_NAME)
+		init_db()
+		print('Struktura baze uspesno inicijalizovana')
+		username, password = None, None
+		if args.auto_init:
+			username, password = 'admin', 'admin12345'
+		else:
+			print(' === UNOS ADMINISTRATOROVOG NALOGA === ')
+			while True:
+				try:
+					username = input('Unesite korisnicko ime: ').strip()
+					KorisnikLogic.validate_username(username)
+					password = getpass.getpass('Unesite lozinku: ').strip()
+					KorisnikLogic.validate_password(password)
+					while True:
+						password_confirmation = getpass.getpass('Potvrdite lozinku: ').strip()
+						if password == password_confirmation:
+							break
+						else:
+							print('Unete lozinke se ne poklapaju, pokusajte ponovo')
+					break
+				except InvalidKorisnikInfoError as e:
+					print(' * Greska:', e)
+					if not input('Zelite li da pokusate ponovo? [D/n] ').strip().upper()\
+								 not in ['N', 'NE']:
+						raise
+		if KorisnikLogic.register_user(username=username, password=password, uloga='admin'):
+			print('Administratorov nalog je uspesno dodat')
+		else:
+			print('Doslo je do greske prilikom dodavanja administratorovog naloga')
+			raise Exception()
+		
+		print('Sistem je inicijalizovan i spreman za koriscenje')
+	except:
+		print('Doslo je do greske prilikom inicijalizacije sistema')
+
+
+
+if __name__ == "__main__":
+	main()
---
+++ views/shared_functionality.py
@@ -0,0 +1,49 @@
+from businesslayer import KorisnikLogic, InvalidKorisnikInfoError
+from utils import clear_screen
+import getpass
+
+def try_again():
+	return input('Zelite li da pokusate ponovo? [D/n] ').strip().upper() not in ['N', 'NE']
+
+
+def change_username(korisnik):
+	while True:
+		clear_screen()
+		print(' === PROMENA KORISNICKOG IMENA === ')
+		try:
+			new_username = input('Unesite novo korisnicko ime: ')
+			success = KorisnikLogic.change_username(korisnik, new_username)
+			if success:
+				print('Uspesno ste promenili korisnicko ime')
+			else:
+				print('Doslo je do greske prilikom promene korisnickog imena, pokusajte kasnije')
+			return
+		except InvalidKorisnikInfoError as e:
+			print(' * Greska:', e)
+			if not try_again():
+				return
+
+
+def change_password(korisnik):
+	while True:
+		clear_screen()
+		print(' === PROMENA LOZINKE === ')
+		try:
+			new_password = getpass.getpass('Unesite novu lozinku: ').strip()
+			KorisnikLogic.validate_password(new_password)
+			while True:
+				password_confirmation = getpass.getpass('Potvrdite lozinku: ').strip()
+				if new_password == password_confirmation:
+					break
+				else:
+					print('Unete lozinke se ne poklapaju, pokusajte ponovo')
+			success = KorisnikLogic.change_password(korisnik, new_password)
+			if success:
+				print('Uspesno ste promenili lozinku')
+			else:
+				print('Doslo je do greske prilikom promene lozinke, pokusajte kasnije')
+			return
+		except InvalidKorisnikInfoError as e:
+			print(' * Greska:', e)
+			if not try_again():
+				return
